e00fd38924baa7439b8cee73fa36ec7bffb4c2e9
Zaid <qzaid7809@gmail.com>
2025-10-29 14:35:35 +0500

    second commite

--- src/components/Olx/Fashion.jsx
+++ src/components/Olx/Fashion.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import image from "../../assets/fashion-beauty.6ef7c1f060c92b55a6b28bfcfb16a1d2.png"
-import styles from "./fashion.module.css"
+import styles from "./Fashion.module.css"
 
 function Fashion() {
   return (
