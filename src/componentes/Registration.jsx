a737567156fc9ee0f9aef52da036091ed8706e67
Zaid <qzaid7809@gmail.com>
2025-10-29 14:31:16 +0500

    first commit

--- src/componentes/Registration.jsx
+++ src/componentes/Registration.jsx
@@ -15,10 +15,11 @@ export default function Registration() {
 
   const onSubmit = async (data) => {
     try {
-      const response = await fetch("http://localhost:5000/api/users/register", {
+      const response = await fetch("http://localhost:8000/api/users/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(data),
+        
       });
 
       const result = await response.json();
